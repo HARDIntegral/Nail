cefd045eadc8666a470ff6e54fc5164cc710e905
HARDIntegral <mohammed.a.tazwar04@gmail.com>
2021-04-18 20:38:34 -0400

    got basic file opening to work

--- src/runner/main.cpp
+++ src/runner/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 
 #define SUCCESS 1
+#define FAILURE 0
 
-int main() {
-  std::cout << "TEST" << std::endl;
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  if (argc < 2 || argc > 2) {
+    cout << "IMPROPER ARGUMENTS" << endl;
+    return FAILURE;
+  }
+  
+  string inputArgs = argv[1];
+  if (inputArgs.substr(inputArgs.length() - 3).compare(".wl") != 0) {
+    cout << "IMPROPER FILE TYPE" << endl;
+    return FAILURE;
+  }
+
+  fstream inputFile;
+  cout << inputArgs << endl;
+  inputFile.open(inputArgs, ios::in);
+  if (!inputFile) 
+    cout << "FILE NOT FOUND" << endl;
+  else {
+    cout << inputArgs << endl;
+
+    // TODO: add the stuff that runs the file
+
+    inputFile.close();
+  }
 
   return SUCCESS;
 }
