07bf35cc44bca3d40eaf0681d19d26ee10cd8fbd
[HARD]Integral <mohammed.a.tazwar04@gmail.com>
2021-04-21 17:17:20 -0400

    Revert "🔥: removed c++"
    This reverts commit 5d38918fc39e70b99ab224c511cb10a151810c6a.

---
+++ src/includes/list.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <iostream>
+
+template <class T>
+struct node {
+  T data;
+  node<T> *next;
+  node<T> *prev;
+};
+
+template <class T>
+class List {
+  public:
+    // Generate a list
+    List();
+    // Destroys list
+    ~List();
+    // Add values to the list
+    int push(T data);
+    int append(T data);
+    // Non-destructive getters
+    T *getHead();
+    T *getTail();
+    T *retriveData(int position);
+    // Destructive getters
+    T *pop();
+    T *snip();
+    T *removeNode(int position);
+    // Other functions
+    int length();
+    void iterate(void (*fn)(T), int reverse);
+  private:
+    node<T> *head;
+    node<T> *tail;
+    int size;
+
+    node<T> *generateNode(T data); 
+    node<T> *retriveNode(int position); 
+};
---
+++ src/includes/list_helpers.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+
+template <typename T>
+void printData(T input) {
+  std::cout << input << " ";
+}
