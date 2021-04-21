36955868c08337936fbffb0a61e5a2ba8555bf17
[HARD]Integral <mohammed.a.tazwar04@gmail.com>
2021-04-21 17:17:27 -0400

    Revert "✨: added Logos lexer"
    This reverts commit 8194e94c9dc69624ef0d1ae270976bfda2982b9f.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,3 @@
-use logos::Logos;
-
-#[derive(Logos, Debug, PartialEq)]
-enum Token {
-    #[token("=")]
-    EQUAL,
-    #[regex(r"-?\d")]
-    INT,
-
-    // error
-    #[error]
-    // skip
-    #[regex(r"[ \t\n\f]+", logos::skip)]
-    Error,
-}
-
 fn main() {
-    let lex = Token::lexer("let a: int = 1 * (1 + 3)");
-    lex.for_each(|i| println!("{:?}", i));
-    // println!("{:?}", lex.next())
+    println!("Hello, world!");
 }
