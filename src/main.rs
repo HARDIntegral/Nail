8194e94c9dc69624ef0d1ae270976bfda2982b9f
okirshen <okirshen@example.com>
2021-04-21 21:01:27 +0300

    ✨: added Logos lexer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use logos::Logos;
+
+#[derive(Logos, Debug, PartialEq)]
+enum Token {
+    #[token("=")]
+    EQUAL,
+    #[regex(r"-?\d")]
+    INT,
+
+    // error
+    #[error]
+    // skip
+    #[regex(r"[ \t\n\f]+", logos::skip)]
+    Error,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let lex = Token::lexer("let a: int = 1 * (1 + 3)");
+    lex.for_each(|i| println!("{:?}", i));
+    // println!("{:?}", lex.next())
 }
