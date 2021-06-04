1eb79df259af1439b7332ce596ac485cd4fbb6e0
Integral <integral@pop-os.localdomain>
2021-06-03 21:21:54 -0500

    hopefully this commit stays

--- parser_lib/parser.c
+++ parser_lib/parser.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
+#include "test.h"
 
 #define SUCCESS 0
 #define FAILURE 1
 
 int parser(char* fileContents) {
     printf("%s\n", fileContents);
-
+    test();
     return SUCCESS;
 }
