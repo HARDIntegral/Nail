eb1245b4bdae8e9e5742e424f268037bb90f2724
Integral <integral@pop-os.localdomain>
2021-06-05 15:02:44 -0400

    starting to clean lines and getting them ready to tokenize

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,33 @@
+#![allow(dead_code)] // for now
+#![allow(unused_variables)] // for now
+#![allow(unused_mut)] // for now
+
+mod types;
+pub struct Token {
+    token_type: types::Types,
+    value: String
+}
+
 pub fn parse(file_contents: String) -> i32 {
-    println!("{}", file_contents);
+    // split the input contents into sperate lines
+    let mut token_list: Vec<Token> = Vec::new();
+    let mut lines: Vec<String> = linefy(file_contents);
+
+    for line in lines.iter() {
+        println!("{}", line);
+    }
+    
     0
+}
+
+fn linefy(input: String) -> Vec<String> {
+    let mut lineified: Vec<String> = Vec::new();
+    let mut raw_lines = input.lines();
+    for mut line in raw_lines {
+        // ignore comments
+        if !line.trim().starts_with("//") {
+            lineified.push(line.trim().to_string());
+        }
+    }
+    lineified
 }
